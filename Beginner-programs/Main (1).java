350085e8926f4060ddfdccad36225055e793258f
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2022-12-08 23:34:32 +0530

    Add files via upload

---
+++ Beginner-programs/Main (1).java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class array
+{
+	public static void main(String[] args) {
+		int age= 30;
+		int physics = 98;
+		int english = 90;
+		
+		int[] marks = new int[0];
+		marks[0]=98;
+		marks[1]=90;
+		
+		System.out.println(marks.length);
+	}
+}

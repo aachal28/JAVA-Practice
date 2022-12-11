0d81a67fd50611278075b1f3e18a74331ade9e9f
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2022-12-11 09:17:26 +0530

    Add files via upload

---
+++ Beginner-programs/concatstr.java
@@ -0,0 +1,8 @@
+public class concatstr{
+      public static void main(String args[])
+{     String name1="aachal";
+      String name2="pardeshi";
+      String name3=name1+name2;
+      System.out.println(name3);
+}
+}

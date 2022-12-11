137b130c7d313a266601668ce57f3d19da86ba29
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2022-12-11 13:30:57 +0530

    Add files via upload

---
+++ Beginner-programs/replacestr.java
@@ -0,0 +1,8 @@
+public class replacestr{
+	public static void main(String[] args)
+{
+	String name= "aman";
+	String replaceString=name.replace ("a","b");
+	System.out.println(replaceString);
+}
+} 
